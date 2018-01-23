9b694215cc30c59cffa4ef0d6a19bffec0a1a224
Grady Stewart <gtstewar@ncsu.edu>
2018-01-22 19:54:37 -0500

    messed around with home.html to display stuff from general and advanced stats

--- src/main/java/com/dynamic/duo/controllers/home/HomePageController.java
+++ src/main/java/com/dynamic/duo/controllers/home/HomePageController.java
@@ -1,6 +1,8 @@
 package com.dynamic.duo.controllers.home;
 
 import com.dynamic.duo.model.persistent.Player;
+import com.dynamic.duo.model.persistent.PlayerAdvancedStats;
+import com.dynamic.duo.model.persistent.PlayerGeneralStats;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +12,11 @@ public class HomePageController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String home(@RequestParam(value="id", defaultValue="1") Long id , Model model) {
         Player player = Player.getByID(id);
-        long idret = Player.getIDFromName(player.getName());
-        System.out.println(idret);
+        PlayerGeneralStats gen = PlayerGeneralStats.getByPlayerID(player.getId());
+        PlayerAdvancedStats adv = PlayerAdvancedStats.getByPlayerID(player.getId());
+        model.addAttribute("points", gen.getPts());
         model.addAttribute("name", player.getName());
+        model.addAttribute("offrat", adv.getOff_rating() );
         return "home";
     }
 }
