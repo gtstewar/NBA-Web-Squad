f682fef6768766b7bea1c813dc1d0f23b05c05db
jmtimper <jmtimper@ncsu.edu>
2018-01-26 16:58:17 -0500

    Created welcome page with random picture and started implementing buttons.

---
+++ src/main/java/com/dynamic/duo/controllers/home/WelcomePageController.java
@@ -0,0 +1,13 @@
+package com.dynamic.duo.controllers.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class WelcomePageController {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String welcome(Model model) {
+        return "welcome";
+    }
+}
