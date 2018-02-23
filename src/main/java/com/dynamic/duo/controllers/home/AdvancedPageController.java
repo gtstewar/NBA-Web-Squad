48b8aec7fb7eb685cc67808b88ede2abda985cb8
jmtimper <jmtimper@ncsu.edu>
2018-02-22 20:59:46 -0500

    #4 added class for leader values and created pages for each header link. Also switched the header to dark

---
+++ src/main/java/com/dynamic/duo/controllers/home/AdvancedPageController.java
@@ -0,0 +1,14 @@
+package com.dynamic.duo.controllers.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AdvancedPageController {
+    @RequestMapping(value = "advanced-stats", method = RequestMethod.GET)
+    public String home( Model model) {
+        return "advanced-stats";
+    }
+}
