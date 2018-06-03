501aa35fa07214dbab3c417693c5d090a0ca79e6
Grady Stewart <gtstewar@ncsu.edu>
2018-06-03 14:32:30 -0400

    added Cors tags to API

--- src/main/java/com/dynamic/duo/controllers/api/PlayerController.kt
+++ src/main/java/com/dynamic/duo/controllers/api/PlayerController.kt
@@ -7,19 +7,27 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import com.google.gson.GsonBuilder
+import org.springframework.web.bind.annotation.CrossOrigin
 
 @RestController
 class PlayerController {
 
-    @GetMapping("/player/{name}")
-    fun getPlayerByName(@PathVariable("name") name: String): ResponseEntity<Player?> {
+    @CrossOrigin(origins = ["http://localhost:4200"])
+    @GetMapping("api/player/{name}")
+    fun getPlayerByName(@PathVariable("name") name: String): ResponseEntity<String> {
         val player: Player? = DomainObject.getBy(Player::class.java, "player_name", name) as Player? ?: return ResponseEntity(HttpStatus.NOT_FOUND)
-        return ResponseEntity(player, HttpStatus.OK)
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        val jsonPlayer: String = gson.toJson(player)
+        return ResponseEntity(jsonPlayer, HttpStatus.OK)
     }
 
-    @GetMapping("/players/top/{attribute}/{number}")
-    fun getTopPlayersByAttribute(@PathVariable("attribute") attribute: String, @PathVariable("number") numOfPlayers: Int): ResponseEntity<Array<Player?>?> {
+    @CrossOrigin(origins = ["http://localhost:4200"])
+    @GetMapping("api/players/top/{attribute}/{number}")
+    fun getTopPlayersByAttribute(@PathVariable("attribute") attribute: String, @PathVariable("number") numOfPlayers: Int): ResponseEntity<String> {
         val players: Array<Player?>? = DomainObject.getTopItems(Player::class.java, attribute, numOfPlayers, true ) as Array<Player?>? ?: return ResponseEntity(HttpStatus.NOT_FOUND)
-        return ResponseEntity(players, HttpStatus.OK)
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        val jsonPlayers: String = gson.toJson(players)
+        return ResponseEntity(jsonPlayers, HttpStatus.OK)
     }
 }
